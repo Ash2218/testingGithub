d9fbce86c4a70b9b44a03afaa282e3d728fe990f
Ash2218 <ashmikaramsaran34@gmail.com>
2026-05-09 14:31:04 +0200

    added btn 2 to the quote form

--- Quote.Designer.cs
+++ Quote.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 114);
+            this.button1.Location = new System.Drawing.Point(96, 55);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 105);
             this.button1.TabIndex = 0;
@@ -41,11 +42,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(359, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(269, 165);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Quote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Quote";
             this.Text = "Quote";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
